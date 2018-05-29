2320538b046e07a0cd76b4a2069df92601b5b270
Michael Le-Reiver <m.lereiver@gmail.com>
2018-05-29 14:40:09 -0700

    added notes-queries.sql

--- db/noteful-app.1.sql
+++ db/noteful-app.1.sql
@@ -1,1 +1,27 @@
 SELECT CURRENT_DATE;
+
+-- DROP TABLE IF EXISTS notes;
+
+-- CREATE TABLE notes(
+--   id serial PRIMARY KEY,
+--   title text NOT NULL,
+--   content text,
+--   created timestamp DEFAULT current_timestamp
+-- );
+
+-- SELECT * FROM notes;
+
+-- INSERT INTO notes (title,content)
+-- VALUES ('Water Plants','My plants are thisrty'),
+--   ('Wash Dishes','My dishes are dirty'),
+--   ('Read about SQL','Learning SQL is fun!'),
+--   ('Call Mom','Mom called yesterday, I need to call her back'),
+--   ('Exercise','You''re out of shape! Go to the Gym') 
+--   RETURNING id,title;
+
+--   INSERT INTO notes (title,content)
+-- VALUES ('Wash Car','My car is dirty'),
+--   ('Brush Teeth','My teeth are dirty. YUK!')
+--   RETURNING id,title;
+
+SELECT * FROM notes;
---
+++ db/notes-queries.sql
@@ -0,0 +1,34 @@
+SELECT * FROM notes;
+
+-- SELECT * FROM notes 
+-- LIMIT 5;
+
+-- SELECT * FROM notes
+-- ORDER BY id;
+
+-- SELECT * FROM notes
+-- ORDER BY title;
+
+-- SELECT * FROM notes
+-- ORDER BY created DESC;
+
+-- SELECT * FROM notes
+-- WHERE title = 'Exercise';
+
+-- SELECT * FROM notes
+-- WHERE title LIKE 'SQL';
+
+-- UPDATE notes
+-- SET title = 'Wash Bike', content = 'My bike is dirty'
+-- WHERE id = 6;
+
+-- INSERT INTO notes (title, content)
+-- VALUES ('Relax', 'You worked hard, now go relax!')
+-- VALUES ('OOPS')
+-- VALUES ('You worked hard, now go relax!');
+
+-- DELETE FROM notes
+-- WHERE id = 8;
+
+
+-- ALTER SEQUENCE notes_id_seq RESTART WITH 1000;
